dec311b41066355525b462b5a2751606fbb4e10f
paras-j <59125176+paras-j@users.noreply.github.com>
2024-12-16 21:03:15 -0500

    Update Logo.tsx

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -5,7 +5,7 @@ import { GraphState } from './playground/Playground'
 const Logo = ({ setGraphState }: { setGraphState: (graphState: GraphState | null) => void }) => {
   return (
     <div className='fixed hover:cursor-pointer top-4 left-4 z-50' onClick={() => setGraphState(null)}>
-      <Image src='/langgraph.svg' alt='LangGraph Logo' width={120} height={40} priority />
+      <Image src='/kikufulllogo.svg' alt='Kiku Logo' width={120} height={40} priority />
     </div>
   )
 }
